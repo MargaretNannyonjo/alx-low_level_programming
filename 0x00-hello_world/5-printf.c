9bf7ae7c22effe0ccbd74640c2091c0c31032712
MargaretNannyonjo <nannyonjoleon@gmail.com>
2023-08-09 23:32:15 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+*main- Entry point
+	*Description:C program that prnts with a new line
+*return always 0 (success)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
