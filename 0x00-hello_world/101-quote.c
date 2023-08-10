0d8b085c010c8aabfdb50b9842f78154f5110980
MargaretNannyonjo <nannyonjoleon@gmail.com>
2023-08-10 05:37:57 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+  * Description:main - Entry point
+  * Return: 1 if success
+  */
+int main(void)
+{
+fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+return (1);
