b9143b72e346c911e3fc9abfd93233af217deb11
MargaretNannyonjo <nannyonjoleon@gmail.com>
2023-08-11 15:55:23 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * Description:main - A program that prints the alphabet in lowercase
+ * Return: 0 if success
+ */
+int main(void)
+{
+int n = 97;
+while (n <= 122);
+{
+putchar(n);
+n++;
+}
+putchar(10);
+return (0);
+}
+
