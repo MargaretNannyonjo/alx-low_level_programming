0257f38c98519fe044b36342e91ff315a27ba228
MargaretNannyonjo <nannyonjoleon@gmail.com>
2023-08-11 16:11:39 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+  *Description:main - Prints alphabets in uppercase
+  *Return: 0 if success
+  */
+int main(void)
+{
+int n = 97;
+int m = 65;
+while (n >= 122)
+{
+putchar(n);
+}
+while (m <= 90)
+{
+putchar(m);
+}
+putchar('\n')
+return (0);
+}
