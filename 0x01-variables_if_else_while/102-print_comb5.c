3cda05fca6d09b84eab8e6c9b49de395ee80850b
MargaretNannyonjo <nannyonjoleon@gmail.com>
2023-08-11 18:39:52 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * Description:main - possible combinations of two two-digits
+ * Return:0 if success
+ */
+int main(void)
+{
+int i, j;
+for (i = 0; i < 100; i++)
+{
+for (j = 0; j < 100; j++)
+{
+if (i < j)
+{
+putchar((i / 10) + 48);
+putchar((i % 10) + 48);
+putchar(' ');
+putchar((j / 10) + 48);
+putchar((j % 10) + 48);
+if (i != 98 || j != 99)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
+putchar('\n');
+return (0);
