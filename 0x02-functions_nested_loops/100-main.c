201f04ed9ae3327d0f5b4cf245b4a43d6cf5cc1d
MargaretNannyonjo <nannyonjoleon@gmail.com>
2023-08-15 16:39:00 -0700

    prints time table

--- 0x02-functions_nested_loops/100-main.c
+++ 0x02-functions_nested_loops/100-main.c
@@ -1,17 +1,12 @@
 #include "main.h"
+
 /**
- * main - check the code.
+ * ptint_times_table - check the code.
  *
  * Return: Always 0.
  */
-int main(void)
+void print_times_table(void)
 {
-print_times_table(3);
-_putchar('\n');
-print_times_table(5);
-_putchar('\n');
-print_times_table(98);
-_putchar('\n');
-print_times_table(12);  
-return (0);
+    print_times_table();
+    _putchar('\n');  
 }
--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,18 +2,17 @@
 #include <stdio.h>
 /**
  * print_times_table - prints the n times table
- * @n: the number for the times table
+ * @n: the number for the times table to be printed
  */
 void print_times_table(int n)
 {
 int i, j, k;
 if (n >= 0 && n <= 15)
-{
 for (i = 0; i <= n; i++)
 {
 for (j = 0; j <= n; j++)
 {
-k = j * i;
+k = i * j;
 if (j == 0)
 {
 _putchar(k +'0');
@@ -46,5 +45,3 @@ _putchar((k % 10) + '0');
 _putchar('\n');
 }
 }
-}
-
