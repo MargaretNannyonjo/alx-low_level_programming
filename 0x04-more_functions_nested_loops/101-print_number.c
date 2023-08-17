05ce87877f3a57027e0edc32a9846a08caa77bfd
MargaretNannyonjo <nannyonjoleon@gmail.com>
2023-08-17 07:09:08 -0700

    prints an integer

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,15 +6,13 @@
  */
 void print_number(int n)
 {
-unsigned long int i = n;
+unsigned int num = n;
 if (n < 0)
 {
-_putchar(45);
-i = -i;
+_putchar('-');
+num = n * -1;
 }
-if (i / 10)
-{
-print_number(i / 10);
-}
-_putchar(i % 10 + '0');
+if (num > 9)
+print_number(num / 10);
+_putchar(num % 10 + '0');
 }
