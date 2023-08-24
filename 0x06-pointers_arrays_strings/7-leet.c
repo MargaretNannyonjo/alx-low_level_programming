ed03e1139dc1a94d139f4e022a3308c768dade5c
MargaretNannyonjo <nannyonjoleon@gmail.com>
2023-08-24 03:37:57 -0700

    encodes a string into 1337.

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,7 +7,7 @@
 char *leet(char *n)
 {
 	int i, j;
-	char s1[] = "aAeeoOtTlL";
+	char s1[] = "aAeEoOtTlL";
 	char s2[] = "4433007711";
 
 	for (i = 0; n[i] != '\0'; i++)
@@ -17,7 +17,6 @@ char *leet(char *n)
 			if (n[i] == s1[j])
 			{
 				n[i] = s2[j];
-				break;
 			}
 		}
 	}
