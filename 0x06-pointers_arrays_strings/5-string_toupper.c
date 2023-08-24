9101255993045d3e0987c4b569c8589983941116
MargaretNannyonjo <nannyonjoleon@gmail.com>
2023-08-24 03:20:39 -0700

    lowercase letters of a string to uppercase.

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -6,16 +6,13 @@
  */
 char *string_toupper(char *n)
 {
-	int i;
-
-	i = 0;
-	while (n[i] != '\0')
-	{
-		if (n[i] >= 'a' && n[i] <= 'z')
-		{
-		    n[i] = n[i] - 32;
-		}
-		    i++;
-	}
-	return (n);
+int i;
+i = 0;
+while (n[i] != '\0')
+{
+if (n[i] >= 'a' && n[i] <= 'z')
+n[i] = n[i] - 32;
+i++;
+}
+return (n);
 }
